009b3e23fd19d2bfeae2fdc5bf0299c27dfd5886
CN\hanshaoqing <shaoqinghan@gmail.com>
2022-05-06 23:50:05 +0800

    todo 处理提案

--- Molochv2.1.sol
+++ Molochv2.1.sol
@@ -43,13 +43,13 @@ contract Moloch is ReentrancyGuard {
     // default = 35 periods (7 days) 默认7天的缓冲期（Grace Period），在此期间，对投票结果不满意的股东可以怒退
     uint256 public gracePeriodLength;
     // default = 10 ETH (~$1,000 worth of ETH at contract deployment)
-    // 提议需要质押的ETH？？
+    // 提案赞助者需要质押的token数量
     uint256 public proposalDeposit;
     // default = 3 - maximum multiplier a YES voter will be obligated to pay in case of mass ragequit
     // 防止大量的怒退情况下，有义务向投票yes的投票者支付（3 - maximum multiplier）
     uint256 public dilutionBound;
     // default = 0.1 - amount of ETH to give to whoever processes a proposal
-    // 奖励给任何处理提议的ETH（0.1 - amount of ETH）
+    // 奖励给任何处理提案的ETH（0.1 - amount of ETH）
     uint256 public processingReward;
     // needed to determine the current period
     // 当前时间戳
@@ -58,7 +58,7 @@ contract Moloch is ReentrancyGuard {
     // 在 eip-1167代理模式下的内部部署最终标志
     bool private initialized;
     // deposit token contract reference; default = wETH
-    // 质押token合约引用
+    // 质押token合约地址
     address public depositToken;
 
     // HARD-CODED LIMITS
@@ -83,22 +83,23 @@ contract Moloch is ReentrancyGuard {
     // ***************
     //
     event SummonComplete(address indexed summoner, address[] tokens, uint256 summoningTime, uint256 periodDuration, uint256 votingPeriodLength, uint256 gracePeriodLength, uint256 proposalDeposit, uint256 dilutionBound, uint256 processingReward);
-    //提交提议
+    //提交提案
     event SubmitProposal(address indexed applicant, uint256 sharesRequested, uint256 lootRequested, uint256 tributeOffered, address tributeToken, uint256 paymentRequested, address paymentToken, string details, bool[6] flags, uint256 proposalId, address indexed delegateKey, address indexed memberAddress);
-    //
+    //赞助提案
     event SponsorProposal(address indexed delegateKey, address indexed memberAddress, uint256 proposalId, uint256 proposalIndex, uint256 startingPeriod);
     //提交投票
     event SubmitVote(uint256 proposalId, uint256 indexed proposalIndex, address indexed delegateKey, address indexed memberAddress, uint8 uintVote);
-    //处理提议
+    //处理提案
     event ProcessProposal(uint256 indexed proposalIndex, uint256 indexed proposalId, bool didPass);
-    //处理白名单提议
+    //处理白名单提案
     event ProcessWhitelistProposal(uint256 indexed proposalIndex, uint256 indexed proposalId, bool didPass);
-    //处理协议踢出协议
+    //处理协议踢出提案
     event ProcessGuildKickProposal(uint256 indexed proposalIndex, uint256 indexed proposalId, bool didPass);
     //怒退事件
     event Ragequit(address indexed memberAddress, uint256 sharesToBurn, uint256 lootToBurn);
+    //
     event TokensCollected(address indexed token, uint256 amountToCollect);
-    //取消体系
+    //取消提案
     event CancelProposal(uint256 indexed proposalId, address applicantAddress);
     //更新成员代理key
     event UpdateDelegateKey(address indexed memberAddress, address newDelegateKey);
@@ -108,11 +109,11 @@ contract Moloch is ReentrancyGuard {
     // *******************
     // INTERNAL ACCOUNTING
     // *******************
-    uint256 public proposalCount = 0; // total proposals submitted
+    uint256 public proposalCount = 0; // total proposals submitted 总共提交的提案数量
     uint256 public totalShares = 0; // total shares across all members 成员总投票份额
     uint256 public totalLoot = 0; // total loot across all members 成员总股份份额
 
-    uint256 public totalGuildBankTokens = 0; // total tokens with non-zero balance in guild bank
+    uint256 public totalGuildBankTokens = 0; // total tokens with non-zero balance in guild bank 公会银行token中数量
 
     address public constant GUILD = address(0xdead); //公会
     address public constant ESCROW = address(0xbeef); //第三方托管, 托管提议奖励的token
@@ -141,7 +142,7 @@ contract Moloch is ReentrancyGuard {
         // highest proposal index # on which the member voted YES 最高的yes提议的索引
         uint256 highestIndexYesVote;
         // set to proposalIndex of a passing guild kick proposal for this member, prevents voting on and sponsoring proposals
-        // 协会踢出成员的协议索引， 阻止投票和发起提议
+        // 公会踢出成员的提案索引， 阻止投票和赞助提案
         uint256 jailed;
     }
     //提案
@@ -183,13 +184,14 @@ contract Moloch is ReentrancyGuard {
         // 总共投NO的票数
         uint256 noVotes;
         // [sponsored, processed, didPass, cancelled, whitelist, guildkick]
-        // 模式，阶段？？？
+        // 提案各节点状态标志
+        // [sponsored(提案是否已赞助), processed（）, didPass（）, cancelled（提案是否取消）, whitelist（token白名单提案）, guildkick（是否为公会踢出成员提案）]
         bool[6] flags;
         // proposal details - could be IPFS hash, plaintext, or JSON
         //提议详情
         string details;
         // the maximum # of total shares encountered at a yes vote on this proposal
-        // 投yes的最大份额和loot
+        // 投yes的最大投票份额与loot份额之和
         uint256 maxTotalSharesAndLootAtYesVote;
         // the votes on this proposal by each member
         // 每个成员的投票
@@ -199,15 +201,15 @@ contract Moloch is ReentrancyGuard {
     mapping(address => bool) public tokenWhitelist;
     address[] public approvedTokens;//授权的token
 
-    mapping(address => bool) public proposedToWhitelist;//提议成员白名单？？
-    mapping(address => bool) public proposedToKick;//踢出提议地址
+    mapping(address => bool) public proposedToWhitelist;//已提议的token地址白名单
+    mapping(address => bool) public proposedToKick;//已踢出公会的应用地址
 
     mapping(address => Member) public members;//成员
     mapping(address => address) public memberAddressByDelegateKey;//成员代理key
 
-    mapping(uint256 => Proposal) public proposals;//提议
+    mapping(uint256 => Proposal) public proposals;//提案
 
-    uint256[] public proposalQueue; //提议队列
+    uint256[] public proposalQueue; //提案队列，提案赞助后添加到队列，在队列中的提案方可投票
 
     //成员check
     modifier onlyMember {
@@ -252,7 +254,7 @@ contract Moloch is ReentrancyGuard {
         require(_approvedTokens.length > 0, "need at least one approved token");
         require(_approvedTokens.length <= MAX_TOKEN_WHITELIST_COUNT, "too many tokens");
         require(_proposalDeposit >= _processingReward, "_proposalDeposit cannot be smaller than _processingReward");//提议押金需要大于提议的奖金
-        
+        //质押token地址
         depositToken = _approvedTokens[0];
         //添加初始成员及相应的股份份额
         for (uint256 i = 0; i < _summoner.length; i++) {
@@ -295,9 +297,9 @@ contract Moloch is ReentrancyGuard {
         uint256 lootRequested, //请求Loot份额
         uint256 tributeOffered, //奖励token的数量
         address tributeToken, // 奖励token地址
-        uint256 paymentRequested,
+        uint256 paymentRequested,//支付token数量
         address paymentToken, // 支付token地址
-        string memory details
+        string memory details //提议详情
     ) public nonReentrant returns (uint256 proposalId) {
         require(sharesRequested.add(lootRequested) <= MAX_NUMBER_OF_SHARES_AND_LOOT, "too many shares requested");
         require(tokenWhitelist[tributeToken], "tributeToken is not whitelisted"); // 需要奖励token为白名单
@@ -323,7 +325,9 @@ contract Moloch is ReentrancyGuard {
         //提议索引
         return proposalCount - 1; // return proposalId - contracts calling submit might want it
     }
-
+    /**
+    * 发起token白名单提案
+    */
     function submitWhitelistProposal(address tokenToWhitelist, string memory details) public nonReentrant returns (uint256 proposalId) {
         require(tokenToWhitelist != address(0), "must provide token address");
         require(!tokenWhitelist[tokenToWhitelist], "cannot already have whitelisted the token");
@@ -335,7 +339,9 @@ contract Moloch is ReentrancyGuard {
         _submitProposal(address(0), 0, 0, 0, tokenToWhitelist, 0, address(0), details, flags);
         return proposalCount - 1;
     }
-
+    /**
+    * 发起踢出成员提案
+    */
     function submitGuildKickProposal(address memberToKick, string memory details) public nonReentrant returns (uint256 proposalId) {
         Member memory member = members[memberToKick];
 
@@ -349,91 +355,107 @@ contract Moloch is ReentrancyGuard {
         return proposalCount - 1;
     }
     /**
-    * TODO
+    * 提交提案
     */
     function _submitProposal(
-        address applicant,
-        uint256 sharesRequested,
-        uint256 lootRequested,
-        uint256 tributeOffered,
-        address tributeToken,
-        uint256 paymentRequested,
-        address paymentToken,
-        string memory details,
-        bool[6] memory flags
+        address applicant, //提议发起者
+        uint256 sharesRequested,//请求股份份额
+        uint256 lootRequested, //请求Loot份额
+        uint256 tributeOffered, //奖励token的数量
+        address tributeToken, // 奖励token地址
+        uint256 paymentRequested,//支付token数量
+        address paymentToken, // 支付token地址
+        string memory details, //提议详情
+        bool[6] memory flags //提案
     ) internal {
+        //创建提案
         Proposal memory proposal = Proposal({
-            applicant : applicant,
-            proposer : msg.sender,
-            sponsor : address(0),
-            sharesRequested : sharesRequested,
-            lootRequested : lootRequested,
-            tributeOffered : tributeOffered,
+            applicant : applicant, //发起提议的应用地址
+            proposer : msg.sender, //提议者
+            sponsor : address(0), //赞助者
+            sharesRequested : sharesRequested, //提议投票份额
+            lootRequested : lootRequested, //提议的loot份额
+            tributeOffered : tributeOffered, //
             tributeToken : tributeToken,
             paymentRequested : paymentRequested,
             paymentToken : paymentToken,
-            startingPeriod : 0,
-            yesVotes : 0,
-            noVotes : 0,
-            flags : flags,
+            startingPeriod : 0, //开始投票间隔
+            yesVotes : 0, //yes投票数
+            noVotes : 0, //no投票数据
+            flags : flags, //提议状态
             details : details,
-            maxTotalSharesAndLootAtYesVote : 0
+            maxTotalSharesAndLootAtYesVote : 0 //投票为yes的最大投票份额和loot份额
         });
-
+        //维护提案索引关系
         proposals[proposalCount] = proposal;
         address memberAddress = memberAddressByDelegateKey[msg.sender];
         // NOTE: argument order matters, avoid stack too deep
+        // 产生提案事件
         emit SubmitProposal(applicant, sharesRequested, lootRequested, tributeOffered, tributeToken, paymentRequested, paymentToken, details, flags, proposalCount, msg.sender, memberAddress);
         proposalCount += 1;
     }
     /**
-    *
+    * 赞助提案
     */
     function sponsorProposal(uint256 proposalId) public nonReentrant onlyDelegate {
         // collect proposal deposit from sponsor and store it in the Moloch until the proposal is processed
+        //确保赞助账户的质押token数量足够
         require(IERC20(depositToken).transferFrom(msg.sender, address(this), proposalDeposit), "proposal deposit token transfer failed");
+        //添加质押token数量到托管账号及总池
         unsafeAddToBalance(ESCROW, depositToken, proposalDeposit);
 
         Proposal storage proposal = proposals[proposalId];
-
+        //检查提案者
         require(proposal.proposer != address(0), 'proposal must have been proposed');
+        //不能被赞助
         require(!proposal.flags[0], "proposal has already been sponsored");
+        //没有被取消
         require(!proposal.flags[3], "proposal has been cancelled");
+        //提案发起应用地址不能被踢出
         require(members[proposal.applicant].jailed == 0, "proposal applicant must not be jailed");
 
         if (proposal.tributeOffered > 0 && userTokenBalances[GUILD][proposal.tributeToken] == 0) {
+            //如果提案奖励的token数量有效，且总公会池的奖励token余额为0， 需要确保公会银行token数量小于最大数量？？？？前置检查
             require(totalGuildBankTokens < MAX_TOKEN_GUILDBANK_COUNT, 'cannot sponsor more tribute proposals for new tokens - guildbank is full');
         }
 
-        // whitelist proposal
-        if (proposal.flags[4]) {
+        // whitelist proposal 白名单提案
+        if (proposal.flags[4]) {//白名单开启，则验证token的白名单
             require(!tokenWhitelist[address(proposal.tributeToken)], "cannot already have whitelisted the token");
             require(!proposedToWhitelist[address(proposal.tributeToken)], 'already proposed to whitelist');
+            //检查白名单token数量
             require(approvedTokens.length < MAX_TOKEN_WHITELIST_COUNT, "cannot sponsor more whitelist proposals");
+            //添加到已赞助的token地址白名单
             proposedToWhitelist[address(proposal.tributeToken)] = true;
 
-        // guild kick proposal
+        // guild kick proposal 公会踢出提案
         } else if (proposal.flags[5]) {
+            //确保之前没有被踢出
             require(!proposedToKick[proposal.applicant], 'already proposed to kick');
+            //已赞助的token地址白名单
             proposedToKick[proposal.applicant] = true;
         }
 
-        // compute startingPeriod for proposal
+        // compute startingPeriod for proposal， 计算提案开始时间startingPeriod
+        //取当前提案队列队尾的提案开始周期startingPeriod和当前提案周期getCurrentPeriod的最大者作为当前提案的开始周期
+        //提案队列队尾的提案开始周期Period，如果提案数为0，则为0， 否则为最后一个提案开始时间startingPeriod+1
         uint256 startingPeriod = max(
             getCurrentPeriod(),
             proposalQueue.length == 0 ? 0 : proposals[proposalQueue[proposalQueue.length.sub(1)]].startingPeriod
         ).add(1);
-
+        //提案开始周期
         proposal.startingPeriod = startingPeriod;
-
+        //获取成员代理key
         address memberAddress = memberAddressByDelegateKey[msg.sender];
+        //更新提案赞助者
         proposal.sponsor = memberAddress;
-
+        //更新提案赞助状态为已赞助
         proposal.flags[0] = true; // sponsored
 
         // append proposal to the queue
+        //添加提案到提案队列
         proposalQueue.push(proposalId);
-        
+        //产生赞助提议事件
         emit SponsorProposal(msg.sender, memberAddress, proposalId, proposalQueue.length.sub(1), startingPeriod);
     }
     /**
@@ -443,42 +465,50 @@ contract Moloch is ReentrancyGuard {
     function submitVote(uint256 proposalIndex, uint8 uintVote) public nonReentrant onlyDelegate {
         address memberAddress = memberAddressByDelegateKey[msg.sender];
         Member storage member = members[memberAddress];
-
+        //检查提案索引的有效性， 只有在提案队列的提案，方可投票
         require(proposalIndex < proposalQueue.length, "proposal does not exist");
         Proposal storage proposal = proposals[proposalQueue[proposalIndex]];
-
+        //检查投票行为null：0，yes:1，no:2
         require(uintVote < 3, "must be less than 3");
         Vote vote = Vote(uintVote);
-
+        //确保投票周期大于提案开始投票的周期， 即开始投票
         require(getCurrentPeriod() >= proposal.startingPeriod, "voting period has not started");
+        //确保投票没有过期
         require(!hasVotingPeriodExpired(proposal.startingPeriod), "proposal voting period has expired");
+        //成员必须没有投票
         require(proposal.votesByMember[memberAddress] == Vote.Null, "member has already voted");
+        //只能投yes或no
         require(vote == Vote.Yes || vote == Vote.No, "vote must be either Yes or No");
-
+        //保存投票信息
         proposal.votesByMember[memberAddress] = vote;
 
-        if (vote == Vote.Yes) {
+        if (vote == Vote.Yes) {//yes
+            //更新投票为yes的投票份额
             proposal.yesVotes = proposal.yesVotes.add(member.shares);
 
             // set highest index (latest) yes vote - must be processed for member to ragequit
             if (proposalIndex > member.highestIndexYesVote) {
+                //更新投yes的最大提案数
                 member.highestIndexYesVote = proposalIndex;
             }
 
             // set maximum of total shares encountered at a yes vote - used to bound dilution for yes voters
             if (totalShares.add(totalLoot) > proposal.maxTotalSharesAndLootAtYesVote) {
+                //更新投yes的最大投票和loot份额
                 proposal.maxTotalSharesAndLootAtYesVote = totalShares.add(totalLoot);
             }
 
-        } else if (vote == Vote.No) {
+        } else if (vote == Vote.No) {//no
+            //更新投no的投票份额
             proposal.noVotes = proposal.noVotes.add(member.shares);
         }
      
         // NOTE: subgraph indexes by proposalId not proposalIndex since proposalIndex isn't set untill it's been sponsored but proposal is created on submission
+        //产生投票事件
         emit SubmitVote(proposalQueue[proposalIndex], proposalIndex, msg.sender, memberAddress, uintVote);
     }
     /**
-     *
+     * TODO
      */
     function processProposal(uint256 proposalIndex) public nonReentrant {
         _validateProposalForProcessing(proposalIndex);
@@ -787,7 +817,7 @@ contract Moloch is ReentrancyGuard {
         require(highestIndexYesVote < proposalQueue.length, "proposal does not exist");
         return proposals[proposalQueue[highestIndexYesVote]].flags[1];
     }
-
+    //投票周期是否过期
     function hasVotingPeriodExpired(uint256 startingPeriod) public view returns (bool) {
         return getCurrentPeriod() >= startingPeriod.add(votingPeriodLength);
     }
@@ -798,29 +828,29 @@ contract Moloch is ReentrancyGuard {
     function max(uint256 x, uint256 y) internal pure returns (uint256) {
         return x >= y ? x : y;
     }
-
+    //获取当前Period， 每periodDuration一个提案
     function getCurrentPeriod() public view returns (uint256) {
         return now.sub(summoningTime).div(periodDuration);
     }
-
+    //提案队列长度
     function getProposalQueueLength() public view returns (uint256) {
         return proposalQueue.length;
     }
-
+    //提案flag
     function getProposalFlags(uint256 proposalId) public view returns (bool[6] memory) {
         return proposals[proposalId].flags;
     }
-
+    //获取用户的token余额
     function getUserTokenBalance(address user, address token) public view returns (uint256) {
         return userTokenBalances[user][token];
     }
-
+    //获取提案的成员投票
     function getMemberProposalVote(address memberAddress, uint256 proposalIndex) public view returns (Vote) {
         require(members[memberAddress].exists, "member does not exist");
         require(proposalIndex < proposalQueue.length, "proposal does not exist");
         return proposals[proposalQueue[proposalIndex]].votesByMember[memberAddress];
     }
-
+    //获取总token数量
     function getTokenCount() public view returns (uint256) {
         return approvedTokens.length;
     }
